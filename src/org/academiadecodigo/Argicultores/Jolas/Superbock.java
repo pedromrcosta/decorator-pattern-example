4a23bf33d289bed5162ffb217a33a5eed1854141
Pedro Costa <pedromrodriguescosta@gmail.com>
2022-03-09 09:13:59 +0000

    simple decorator pattern

---
+++ src/org/academiadecodigo/Argicultores/Jolas/Superbock.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.Argicultores.Jolas;
+
+public class Superbock extends Jola{
+
+    public Superbock() {
+        super.setDescription("SUPERBOCK>SAGRES");
+    }
+
+    @Override
+    public double cost() {
+        return 2410;
+    }
+}
